f2f3f21ca7bb74d6908ba680dbb912ec0635412f
Spacehuhn <github@spacehuhn.de>
2021-12-19 23:12:47 +0100

    Digispark compiler

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
 import * as React from 'react';
 //import ReactDOM from 'react-dom';
 import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
 
-function App() {
-  return <Button variant="contained">Hello World</Button>;
+import * as digispark from './digispark/digispark.js';
+
+const App = () => {
+  const [input, setInput] = React.useState("");
+  const [output, setOutput] = React.useState("");
+
+  const convert_digispark = () => {
+    setOutput(digispark.convert(input));
+  };
+
+  const handle_new_input = (event) => {
+    setInput(event.target.value);
+  };
+
+  return (
+    <div>
+      <TextField
+        multiline
+        fullWidth
+        onChange={handle_new_input}
+        minRows={4}
+      />
+      <Button variant="contained" onClick={convert_digispark}>
+        Convert
+      </Button>
+      <TextField
+        multiline
+        fullWidth
+        value={output}
+      />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/digispark/digispark_layouts.js
@@ -0,0 +1,5 @@
+import { digispark_layout_us } from './digispark_layout_us.js'
+
+export const digispark_layouts = {
+    "US": digispark_layout_us,
+};
