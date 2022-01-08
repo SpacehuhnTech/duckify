d6eb25dc63d6c3668765d2525aa79490ce451023
Spacehuhn <github@spacehuhn.de>
2022-01-08 12:10:28 +0100

    Added US layout

--- src/App.js
+++ src/App.js
@@ -4,32 +4,27 @@ import Box from '@mui/material/Box'
 import Button from '@mui/material/Button'
 import TextField from '@mui/material/TextField'
 import Typography from '@mui/material/Typography'
+import InputLabel from '@mui/material/InputLabel'
+import MenuItem from '@mui/material/MenuItem'
+import FormControl from '@mui/material/FormControl'
+import Select from '@mui/material/Select'
 
 import version from './version.js'
 import digisparkConverter from './modules/digispark.js'
 
 import locale_de from './layouts/de.json'
-
-const cleanLayout = (keys) => {
-  /*const newKeyList = []
-
-  keys.forEach(key => {
-    if (key.char !== key.us || key.alt || key.altGr || key.shift || key.combo !== '') {
-      newKeyList.push(key)
-    }
-  })
-
-  return newKeyList*/
-  return keys
-}
+import locale_us from './layouts/us.json'
 
 const App = () => {
   const [input, setInput] = React.useState('')
   const [output, setOutput] = React.useState('')
-
-  const layout = cleanLayout(locale_de.keys)
+  const [layoutStr, setLayoutStr] = React.useState('de')
 
   const convert_digispark = () => {
+    let layout = []
+    if(layoutStr === 'us')  layout = locale_us.keys
+    if(layoutStr === 'de') layout = locale_de.keys
+
     setOutput(digisparkConverter(input, layout, version))
   }
 
@@ -49,6 +44,18 @@ const App = () => {
       <Button variant='contained' onClick={convert_digispark}>
         Convert
       </Button>
+
+      <FormControl sx={{minWidth: 120}}>
+        <InputLabel>Layout</InputLabel>
+        <Select
+          value={layoutStr}
+          label="Layout"
+          onChange={e => setLayoutStr(e.target.value)}
+        >
+          <MenuItem value={'us'}>US</MenuItem>
+          <MenuItem value={'de'}>DE</MenuItem>
+        </Select>
+      </FormControl>
 
       <TextField
         multiline
