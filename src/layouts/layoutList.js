00cca4630aba2a6c93fde2b50c0e1a316e051f0a
Spacehuhn <github@spacehuhn.de>
2022-01-11 14:51:57 +0100

    Added ES layout

--- src/layouts/layoutList.js
+++ src/layouts/layoutList.js
@@ -2,12 +2,14 @@ import locale_us from './us.json'
 import locale_de from './de.json'
 import locale_ro from './ro.json'
 import locale_it from './it.json'
+import locale_es from './es.json'
 
 const layoutList = {
     'us': locale_us,
     'de': locale_de,
     'ro': locale_ro,
     'it': locale_it,
+    'es': locale_es,
 }
 
 export default layoutList
