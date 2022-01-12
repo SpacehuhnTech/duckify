de6a43bb6f3457adb43ce120f83bf2133e5c8ec9
Spacehuhn <github@spacehuhn.de>
2022-01-12 14:38:41 +0100

    Added SK layout

--- src/layouts/layoutList.js
+++ src/layouts/layoutList.js
@@ -4,6 +4,7 @@ import locale_ro from './ro.json'
 import locale_it from './it.json'
 import locale_es from './es.json'
 import locale_si from './si.json'
+import locale_sk from './sk.json'
 
 const layoutList = {
     'us': locale_us,
@@ -12,6 +13,7 @@ const layoutList = {
     'it': locale_it,
     'es': locale_es,
     'si': locale_si,
+    'sk': locale_sk,
 }
 
 export default layoutList
