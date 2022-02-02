9ec7d477252664a0c579293ce7b4950e3a40488f
Spacehuhn <github@spacehuhn.de>
2022-02-02 14:08:42 +0100

    Add link to docs

--- src/Header.js
+++ src/Header.js
@@ -3,6 +3,8 @@ import React from 'react'
 import AppBar from '@mui/material/AppBar'
 import Toolbar from '@mui/material/Toolbar'
 import Typography from '@mui/material/Typography'
+import Button from '@mui/material/Button'
+import InfoIcon from '@mui/icons-material/Info'
 
 const Header = (props) => {
     return (
@@ -18,9 +20,19 @@ const Header = (props) => {
                     variant='h6'
                     component='h1'
                     noWrap
+                    sx={{ flexGrow: 1 }}
                 >
                     &nbsp;Duckify
                 </Typography>
+
+                <Button
+                    //variant='contained'
+                    //color='success'
+                    sx={{ color: '#fff' }}
+                    href='https://docs.duckify.huhn.me'
+                    startIcon={<InfoIcon />}>
+                    Help
+                </Button>
             </Toolbar>
         </AppBar>
     )
