ebe0fa6589d756e9d13a7a29c8fbb03ec6b9ba60
Spacehuhn <github@spacehuhn.de>
2022-05-12 09:15:32 +0200

    Resize textarea

--- src/components/TextArea.js
+++ src/components/TextArea.js
@@ -3,40 +3,31 @@ import React from 'react'
 import Box from '@mui/material/Box'
 import Grid from '@mui/material/Grid'
 import Typography from '@mui/material/Typography'
-import TextField from '@mui/material/TextField'
 import IconButton from '@mui/material/IconButton'
 import DownloadIcon from '@mui/icons-material/Download'
 import ContentCopyIcon from '@mui/icons-material/ContentCopy'
 
 const TextArea = (props) => {
+
     return (
         <Box>
-            <Typography
-                variant='overline'
-                component='h2'
-                align='center'
-                sx={{
-                    marginBottom: '-2.5em',
-                    color: '#aaa',
-                }}>
+            <label for="textarea" style={{
+                color: '#aaa',
+                display: 'block',
+                textAlign: 'center',
+            }}>
                 {props.title}
-            </Typography>
-
-            <TextField
-                multiline
-                fullWidth
-                onChange={(e) => props.setValue(e.target.value)}
-                minRows={14}
-                maxRows={14}
-                value={props.value}
-                InputProps={{
-                    style: {
-                        paddingTop: 20,
-                        paddingRight: 1,
-                        paddingBottom: 5,
-                    }
-                }}
-            />
+                <textarea
+                    onChange={(e) => props.setValue(e.target.value)}
+                    value={props.value}
+                    style={{
+                        minHeight: '10rem',
+                        height: 'calc(100vh - 208px)',
+                        width: '100%',
+                        resize: 'none',
+                    }}
+                />
+            </label>
 
             <Grid
                 container
